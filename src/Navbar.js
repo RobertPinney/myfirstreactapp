1111b64b0fc532b2558b8dfa0adf77c6adaef727
Robert <pinneyr@hotmail.com>
2023-09-12 13:15:09 +0200

    Net Ninja tutorial 5

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+    return ( 
+        <nav className='navbar'> 
+            <h1>NAV</h1>
+            <div className='links'>
+                <a href="/">Home</a>
+                <a href="/contact"></a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
