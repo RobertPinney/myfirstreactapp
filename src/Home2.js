6714c5497f1fa8d87720a6147720296dab9b8ee6
Robert <pinneyr@hotmail.com>
2023-11-28 15:54:55 +0100

    cleanup

--- src/Home2.js
+++ src/Home2.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 
 function Home2() {
@@ -7,9 +7,26 @@ function Home2() {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+
+    // This function is being passed as a prop
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    // Useful hook for when you want to run code with every single render, could for example run authentication software
+    // If an empty array, [] , is included ('a dependency array') at the end just before the final parenthese, this will cause the useeffect hook to only run at the initial render
+    // If the array contains any state values (such as 'blogs' above) then the useEffct will only run if that state is invoked.
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(blogs);
+    });
+
     return (
         <div className="home2">
-            <BlogList blogs={blogs} title='All blogs!'/>
+            <BlogList blogs={blogs} title='All blogs!' handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/>
         </div>
     );
 }
